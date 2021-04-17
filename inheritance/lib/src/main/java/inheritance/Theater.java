4f777de21442b0971c92b70293d27b9d38b9065d
anasswalha95 <anas.swalha@yahoo.com>
2021-04-17 16:27:04 +0300

    added movies review feature task

--- inheritance/lib/src/main/java/inheritance/Theater.java
+++ inheritance/lib/src/main/java/inheritance/Theater.java
@@ -8,44 +8,21 @@ import java.util.LinkedList;
 public class Theater implements ReviewInterface{
 
     private String name;
-
-
-
-
-    private LinkedList<Review> allReviews = new LinkedList<>();
+    private LinkedList<Review> allTheaterReviews = new LinkedList<>();
+    private LinkedList<Review> allMoviesReviews = new LinkedList<>();
     private LinkedList<String> allMovies = new LinkedList<>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-        public String getMovies() {
-        return Arrays.toString(this.allMovies.toArray());
-    }
-
-    public void setMovie(String movie) {
-        this.allMovies.add(movie);
-    }
     //constructor
     public Theater(String name) {
         setName(name);
-
+    }
+    //add movie
+    public void addMovie(String movie) {
+        this.allMovies.add(movie);
     }
 
-
-    @Override
-    public void addReview (Review review){
-        review.setName(this.getName());
-        this.allReviews.add(review);
-    }
-
-
+    //remove movie
     public boolean removeMovie (String movie){
-
         if(allMovies.size() == 0) {
             System.out.println("There are no movies Today");
             return false;
@@ -55,12 +32,52 @@ public class Theater implements ReviewInterface{
         }
 
     }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getMovies() {
+        return Arrays.toString(this.allMovies.toArray());
+    }
+
+
+
+@Override
+    public void addReview (Review review ){
+        review.setName(this.getName());
+        this.allTheaterReviews.add(review);
+    }
+
+
+
+
+    public boolean addReview (Review review ,String movieName){
+        if(allMovies.contains(movieName)) {
+            review.setName(movieName);
+            this.allMoviesReviews.add(review);
+            return true;
+        }else{
+            System.out.println("The reviewed movie does not exists in "+ this.getName());
+            return false;
+        }
+    }
+
+
+    
 
     public String toString() {
         return String.format(
-                "The Movies are %s in Theater %s",
-                this.getMovies(), this.getName());
+                "The Movies are %s in Theater %s and the reviewed movies are %s",
+                this.getMovies(), this.getName(), allMoviesReviews);
     }
+
+
 }
 
-   // JNI error has occurred, please check your installation and try again
+
